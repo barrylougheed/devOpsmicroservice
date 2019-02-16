f0217dc078c9b3ecfc679f0afcc488da410c531e
barry lougheed <barrylougheed43@hotmail.com>
2019-02-16 20:18:49 +0000

    endpoints

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    //Decorates the class with the [ApiController] attribute. 
+    //This attribute indicates that the controller responds to web API requests
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            //Adds an item named "points" if the DB is empty
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.        
+                _context.TodoItems.Add(new TodoItem { Name = "Points" });
+                _context.SaveChanges();
+            }
+        }
+
+        // ENDPOINT GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // ENDPOINT GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+    }
+}
--- TodoApi/Controllers/ValuesController.cs
+++ TodoApi/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[todo]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
