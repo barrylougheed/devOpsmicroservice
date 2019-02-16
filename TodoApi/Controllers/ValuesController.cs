f25299f8781e0e9625d653d6f6de549befa6c5df
barry lougheed <barrylougheed43@hotmail.com>
2019-02-16 20:25:48 +0000

    endpoint error fix

--- TodoApi/Controllers/ValuesController.cs
+++ TodoApi/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[todo]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
